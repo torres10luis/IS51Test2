f0312de99c22100ba5f6407afc6a65c1a27b6a34
luis saldana <tluis10a@gmail.com>
2020-12-03 22:54:10 -0800

    structured english

---
+++ IS51test2.py
@@ -0,0 +1,8 @@
+""" 
+We will write a program that will display the number of grades 
+using the formula open() method to retrieve it from the final.txt file 
+it will also tell us about the average of the grades using the kickstart function
+then it will show the percentage of grades that are above the average grade from the total grades
+then we will use calculate percent above average function to make calculations
+we will then assign grades to a list of intergers to be able to get the number of grades with an average grade and also the percentage
+"""
